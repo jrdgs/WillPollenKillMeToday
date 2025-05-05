5d939ec9d195985a536fa43e11e90d4ebb2c105d
John Rodrigues <rodrigues.joh@northeastern.edu>
2025-05-05 16:09:59 -0400

    Fix bugs after directory mv

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import glob from "glob";
 
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({  
+	plugins: [react()],
+});
 
